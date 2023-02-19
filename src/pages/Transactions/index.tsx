39534f6749197710fbfd614baf5f3fb76181c345
Eduardo Ferraz <edupferraz@gmail.com>
2023-02-19 11:43:00 -0300

    Table Transactions

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,13 +1,53 @@
 import { Header } from "../../components/Header"
 import { Summary } from "../../components/Summary"
-import { TransactionsContainer } from "./styles"
+import { PriceHighlight, TransactionsContainer, TransactionTable } from "./styles"
 
 export function Transactions() {
     return (
-        <TransactionsContainer>
+        <div>
             <Header/>
-
             <Summary/>
-        </TransactionsContainer>
+
+            <TransactionsContainer>
+                <TransactionTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de site</td>
+                            <td>
+                                <PriceHighlight variant="income">
+                                    R$ 12.000,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Venda</td>
+                            <td>19/02/2023</td>
+                        </tr>
+
+                        <tr>
+                            <td width="50%">Hamburguer</td>
+                            <td>
+                                <PriceHighlight variant="outcome">
+                                   - R$ 59,90
+                                </PriceHighlight>
+                            </td>
+                            <td>Alimentação</td>
+                            <td>19/02/2023</td>
+                        </tr>
+
+                        <tr>
+                            <td width="50%">Aluguel do apartamento</td>
+                            <td>
+                                <PriceHighlight variant="outcome">
+                                   - R$ 1200,00
+                                </PriceHighlight>
+                            </td>
+                            <td>Casa</td>
+                            <td>19/02/2023</td>
+                        </tr>
+                    </tbody>
+                </TransactionTable>   
+            </TransactionsContainer>    
+
+
+        </div>
     )
-}
+}   
