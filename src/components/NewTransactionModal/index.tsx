b41779bf78bc1f3978987e02bd6f04db909aff8a
Eduardo Ferraz <edupferraz@gmail.com>
2023-02-20 11:11:42 -0300

    Finish Front-end

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,7 @@
 import * as Dialog from '@radix-ui/react-dialog'
-import { X } from 'phosphor-react'
-import { CloseButton, Content, Overlay } from './style'
+import * as RadioGroup from '@radix-ui/react-radio-group';
+import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './style'
 
 export function NewTransactionModal(){
     return (
@@ -18,6 +19,18 @@ export function NewTransactionModal(){
                     <input type="text" placeholder="Descrição" required />
                     <input type="number" placeholder="Preço" required />
                     <input type="text" placeholder="Categoria" required />
+
+                    <TransactionType>
+                        <TransactionTypeButton variant='income' value='income'>
+                            <ArrowCircleUp size={24}/>
+                            Entrada
+                        </TransactionTypeButton>
+
+                        <TransactionTypeButton variant='outcome' value='outcome'>
+                            <ArrowCircleDown size={24}/>
+                            Saída
+                        </TransactionTypeButton>
+                    </TransactionType>
 
                     <button type='submit'>
                         Cadastrar
