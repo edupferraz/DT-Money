5f99e0a4afc0481dc260a8503e3b80c5890912fe
Eduardo Ferraz <edupferraz@gmail.com>
2024-10-01 06:49:34 -0300

    Create transactions context

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,31 +1,16 @@
-import { useEffect, useState } from "react";
+import { useContext} from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
+import { TransactionsContext } from "../../contexts/TransactionsContext";
 
-interface Transaction {
-    id: number;
-    description: string;
-    type: 'income' | 'outcome';
-    price: number;
-    category: string;
-    createdAt: string;
-}
+
 
 export function Transactions() {
-    const [transactions, setTransactions] = useState<Transaction[]>([])
+    const { transactions } = useContext(TransactionsContext);
 
-    async function loadTransactions() {
-        const response = await fetch('http://localhost:333/transactions')
-        const data = await response.json();
-
-        setTransactions(data)
-    }
-
-    useEffect(() => {
-        loadTransactions();
-    }, [])
+    console.log(transactions)
 
     return (
         <div>
